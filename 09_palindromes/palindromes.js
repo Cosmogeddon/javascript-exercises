0db30c54523bf554e173d877b308cf5341952a61
Cody kelderman <codykelderman@gmail.com>
2023-06-01 14:27:55 -0700

    finish palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    return string.toLowerCase().replace(/[\s"'.,-\/#!$%\^&*;:{}=\-_`~()\\\[\]@+|?><]/g,"") === string.toLowerCase().replace(/[\s"'.,-\/#!$%\^&*;:{}=\-_`~()\\\[\]@+|?><]/g,"").split('').reverse().join('');
 };
 
 // Do not edit below this line
