53074365f094f75a9f4a41af11efa524c8d30cb6
Cody kelderman <codykelderman@gmail.com>
2023-06-01 15:02:55 -0700

    finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(n) {
+        const result = [0, 1];
+        for (var i = 2; i <= n; i++) {
+          const a = result[i - 1];
+          const b = result[i - 2];
+          result.push(a + b);
+        }
+        console.log(result);
+        return result[n];
+      
+      }
 // Do not edit below this line
 module.exports = fibonacci;
